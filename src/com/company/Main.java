8b4d7c526d427b2d2abd42088b18554481531b78
Ann_Savitskay <ansavitskayloki@gmail.com>
2021-09-07 10:51:51 +0300

    study project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello world");
         System.out.println("Nice t meet you");
+        System.out.println("Swamp");
     }
 }
