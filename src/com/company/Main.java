2fa4b5236c32c5d5b634fa6e0bbd8ffa4cf2cd36
Ann_Savitskay <ansavitskayloki@gmail.com>
2021-09-07 11:41:54 +0300

    study project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,17 @@ package com.company;
 
 public class Main {
 
+    static int[] a = {10, 11, 12, 13};
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello world");
         System.out.println("Nice t meet you");
         System.out.println("Swamp");
+
+        for(int i = 0; i < a.length; i++) {
+
+            System.out.println(a[i]);
+            System.out.println("25");
+        }
     }
 }
